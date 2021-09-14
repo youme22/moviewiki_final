6deebdf2f300a65d93c71b97a9e3a7f9acfe3350
hmk219 <hmkreator219@gmail.com>
2021-09-14 15:19:57 +0900

    RecommendServiceImpl 클래스 doReview 메소드 수정

--- src/main/java/com/moviewiki/api/review/service/ReviewServiceImpl.java
+++ src/main/java/com/moviewiki/api/review/service/ReviewServiceImpl.java
@@ -2,14 +2,10 @@ package com.moviewiki.api.review.service;
 
 import com.moviewiki.api.like.domain.Like;
 import com.moviewiki.api.like.repository.LikeRepository;
-import com.moviewiki.api.prefActor.domain.PrefActor;
-import com.moviewiki.api.prefActor.repository.PrefActorRepository;
-import com.moviewiki.api.prefDirector.domain.PrefDirector;
-import com.moviewiki.api.prefDirector.repository.PrefDirectorRepository;
-import com.moviewiki.api.prefGenre.domain.PrefGenre;
-import com.moviewiki.api.prefGenre.repository.PrefGenreRepository;
-import com.moviewiki.api.prefNation.domain.PrefNation;
-import com.moviewiki.api.prefNation.repository.PrefNationRepository;
+import com.moviewiki.api.prefActor.Service.PrefActorService;
+import com.moviewiki.api.prefDirector.service.PrefDirectorService;
+import com.moviewiki.api.prefGenre.service.PrefGenreService;
+import com.moviewiki.api.prefNation.Service.PrefNationService;
 import com.moviewiki.api.review.domain.Review;
 import com.moviewiki.api.review.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,29 +16,21 @@ public class ReviewServiceImpl implements ReviewService {
 
     private ReviewRepository reviewRepository;
     private LikeRepository likeRepository;
-    private PrefGenreRepository prefGenreRepository;
-    private PrefNationRepository prefNationRepository;
-    private PrefDirectorRepository prefDirectorRepository;
-    private PrefActorRepository prefActorRepository;
 
     @Autowired
-    public ReviewServiceImpl(ReviewRepository reviewRepository, LikeRepository likeRepository, PrefGenreRepository prefGenreRepository, PrefNationRepository prefNationRepository, PrefDirectorRepository prefDirectorRepository, PrefActorRepository prefActorRepository) {
+    public ReviewServiceImpl(ReviewRepository reviewRepository, LikeRepository likeRepository) {
         this.reviewRepository = reviewRepository;
         this.likeRepository = likeRepository;
-        this.prefGenreRepository = prefGenreRepository;
-        this.prefNationRepository = prefNationRepository;
-        this.prefDirectorRepository = prefDirectorRepository;
-        this.prefActorRepository = prefActorRepository;
     }
 
 
     @Override
     public void doReview(Review review) {
         reviewRepository.saveReview(review); // 리뷰 등록
-        prefGenreService.updatePrefGenre(prefGenre); // 장르 선호도 업데이트
-        prefNationService.savePrefNation(prefNation); // 국가 선호도 업데이트
-        prefDirectorService.savePrefDirector(prefDirector); // 감독 선호도 업데이트
-        prefActorService.savePrefActor(prefActor); // 배우 테이블 업데이트
+        PrefGenreService.updatePrefGenre(review); // 장르 선호도 업데이트
+        PrefNationService.updatePrefNation(review); // 국가 선호도 업데이트
+        PrefDirectorService.updatePrefDirector(review); // 감독 선호도 업데이트
+        PrefActorService.updatePrefActor(review); // 배우 테이블 업데이트
     }
 
     @Override
