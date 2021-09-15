3197844d8dd1362fa7283bb7c86b020fb5c221fb
minji128 <minji.lee128@gmail.com>
2021-09-15 09:54:43 +0900

    1. DirectorController, DirectorRepository, DirectorService 기능 추가 2. Director 도메인 애노테이션 추가

--- src/main/java/com/moviewiki/api/director/repository/DirectorRepository.java
+++ src/main/java/com/moviewiki/api/director/repository/DirectorRepository.java
@@ -1,7 +1,14 @@
 package com.moviewiki.api.director.repository;
 
+import com.moviewiki.api.actor.domain.Actor;
 import com.moviewiki.api.director.domain.Director;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface DirectorRepository extends JpaRepository<Director, String> {
+    // 특정 배우 찾기 -> 이름으로
+    public List<Director> findByDirectorName(String actorName);
 }
