6924e81539598d4a4ff3642149a624cde00fb465
minji128 <minji.lee128@gmail.com>
2021-09-15 09:56:19 +0900

    애노테이션 추가 및 GenreServiceImpl 기능 구현

--- src/main/java/com/moviewiki/api/genre/repository/GenreRepository.java
+++ src/main/java/com/moviewiki/api/genre/repository/GenreRepository.java
@@ -2,6 +2,8 @@ package com.moviewiki.api.genre.repository;
 
 import com.moviewiki.api.genre.domain.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, String> {
 }
