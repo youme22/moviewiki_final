aa2af219dcb8bed220d0f4b4dd5c035fb3052316
minji128 <minji.lee128@gmail.com>
2021-09-16 16:50:32 +0900

    계절과 날씨 컨트롤러, 서비스, 레파지토리 기능구현

---
+++ src/main/java/com/moviewiki/api/weather/controller/WeatherController.java
@@ -0,0 +1,56 @@
+package com.moviewiki.api.weather.controller;
+
+import com.moviewiki.api.movie.domain.Movie;
+import com.moviewiki.api.movie.service.MovieServiceImpl;
+import com.moviewiki.api.weather.domain.Weather;
+import com.moviewiki.api.weather.service.WeatherServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+
+@Controller
+public class WeatherController {
+
+    @Autowired
+    WeatherServiceImpl weatherServiceImpl;
+
+    @Autowired
+    MovieServiceImpl movieServiceImpl;
+
+    // 날씨점수 등록 페이지 이동
+    @GetMapping("/weather/create")
+    public String createWeather(){
+        return "member/weather_add";
+    }
+
+    // 영화에 대한 날씨점수 등록
+    @PostMapping("/weather/create")
+    public String createSeason(HttpServletRequest request){
+        Long movieId = parseLong(request.getParameter("movieId"));
+        String weatherName = request.getParameter("weatherName");
+        int weatherPoint = parseInt(request.getParameter("weatherPoint"));
+
+        Movie movie = movieServiceImpl.findByMovieId(movieId);
+
+        weatherServiceImpl.save(movie, weatherName, weatherPoint);
+
+        return "admin_movie";
+    }
+
+    // 특정 영화의 계절점수 조회
+    @GetMapping("/weather/read/{weatherName}")
+    @ResponseBody
+    public List<Weather> readWeather(@PathVariable String weatherName){
+        return weatherServiceImpl.findByWeatherNameOrderByWeatherPointDesc(weatherName);
+    }
+
+}
