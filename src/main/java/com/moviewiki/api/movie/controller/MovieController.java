6c9ac0d49b204031ce27182e35e765c54bfef3a3
minji128 <minji.lee128@gmail.com>
2021-09-16 16:53:28 +0900

    영화 주석 및 페이지 이동 변경

--- src/main/java/com/moviewiki/api/movie/controller/MovieController.java
+++ src/main/java/com/moviewiki/api/movie/controller/MovieController.java
@@ -6,7 +6,6 @@ import com.moviewiki.api.movie.domain.MovieForm;
 import com.moviewiki.api.movie.service.MovieServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +26,7 @@ public class MovieController {
     /* 영화 메인 페이지 이동 (임시) */
     @GetMapping("/movie")
     public String Main(){
-        return "admin/admin_movie";
+        return "admin_movie";
     }
 
     /* 영화 등록 페이지 이동 */
