19fcec632bad48eabad624bc5dc8c60e9bab5a40
minji128 <minji.lee128@gmail.com>
2021-09-16 16:52:15 +0900

    보고싶은 영화 기능 구현 및 오류해결

--- src/main/java/com/moviewiki/api/wantToSee/service/WantToSeeServiceImpl.java
+++ src/main/java/com/moviewiki/api/wantToSee/service/WantToSeeServiceImpl.java
@@ -1,10 +1,15 @@
 package com.moviewiki.api.wantToSee.service;
 
+import com.moviewiki.api.movie.domain.Movie;
+import com.moviewiki.api.user.domain.User;
 import com.moviewiki.api.wantToSee.domain.WantToSee;
+import com.moviewiki.api.wantToSee.domain.WantToSeePK;
 import com.moviewiki.api.wantToSee.repository.WantToSeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -13,20 +18,32 @@ public class WantToSeeServiceImpl implements WantToSeeService{
     @Autowired
     private WantToSeeRepository wantToSeeRepository;
 
+    @Autowired
+    EntityManager em;
+
+    /* 보고픈 영화 추가 */
     @Override
-    public void save(WantToSee wantToSee) {
-        wantToSeeRepository.save(wantToSee);
+    @Transactional
+    public void save(User user, Movie movie) {
+        WantToSee wantToSee = new WantToSee();
+        wantToSee.setUser(user);
+        wantToSee.setMovie(movie);
+
+        em.persist(wantToSee);     // 복합키 저장
     }
 
-    /* 미완성!!! */
+    /* 보고픈 영화 조회 -> 사용자 아이디 기준으로 */
     @Override
-    public List<WantToSee> findByUser(String userId) {
-        return wantToSeeRepository.findByUser(userId);
+    public List<WantToSee> findByUser(User user) {
+        return wantToSeeRepository.findByUser(user);
     }
 
-    /* 미완성!!! */
+    /* 보고픈 영화 삭제 */
     @Override
-    public void deleteByUserAndMovie(String userId, Long movieId) {
-        wantToSeeRepository.deleteByUserAndMovie(userId, movieId);
+    @Transactional
+    public void deleteWantToSee(User user, Movie movie) {
+        WantToSeePK wantToSeePK = new WantToSeePK(movie, user);
+        WantToSee wantToSee = em.find(WantToSee.class, wantToSeePK);
+        em.remove(wantToSee);   // 삭제
     }
 }
